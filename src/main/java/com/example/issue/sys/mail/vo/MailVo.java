5b48d77723c66e9db980475094aee7e92d338a98
nihao <nihao@112.172.131.131>
2023-02-21 14:53:40 +0900

    메일

--- src/main/java/com/example/issue/sys/mail/vo/MailVo.java
+++ src/main/java/com/example/issue/sys/mail/vo/MailVo.java
@@ -6,11 +6,31 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter @Data
+
 public class MailVo {	 
 
 	  private String sendName;
 	  private String sendEmail;
 	  private String sendMessage;
+	  
+	public String getSendName() {
+		return sendName;
+	}
+	public void setSendName(String sendName) {
+		this.sendName = sendName;
+	}
+	public String getSendEmail() {
+		return sendEmail;
+	}
+	public void setSendEmail(String sendEmail) {
+		this.sendEmail = sendEmail;
+	}
+	public String getSendMessage() {
+		return sendMessage;
+	}
+	public void setSendMessage(String sendMessage) {
+		this.sendMessage = sendMessage;
+	}
+	  
 
 }
